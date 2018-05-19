19553ad16f83f418872f39398e9864f86285ee93
Jami Gibbs <jami0821@gmail.com>
2018-05-19 14:46:12 -0500

    Adding basic header style

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,18 +3,23 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/campuses">Campuses</Link>
-        </li>
-        <li>
-          <Link to="/students">Students</Link>
-        </li>
-      </ul>
+    <div className="row" id="main-header">
+
+      <div className="column logo">
+        <Link to="/">Interplanetary Academy of JavaScript</Link>
+      </div>
+
+      <div className="column">
+        <ul className="nav">
+          <li>
+            <Link to="/campuses">Campuses</Link>
+          </li>
+          <li>
+            <Link to="/students">Students</Link>
+          </li>
+        </ul>
+      </div>
+
     </div>
   )
 }
--- app/components/index.js
+++ app/components/index.js
@@ -9,7 +9,7 @@ import Header from './Header'
 const Main = () => {
   return (
     <Router>
-      <div>
+      <div className="container root-container">
         <Header />
         <Route exact path="/" component={Home} />
         <Route path="/campuses" component={Campuses} />
