3b0a6bdc40f7c85e16e5f5400e0f49ca999181c1
Jami Gibbs <jami0821@gmail.com>
2018-05-19 10:03:16 -0500

    Adding try/catch to routes

--- server/api/campuses.js
+++ server/api/campuses.js
@@ -2,18 +2,23 @@ const router = require('express').Router();
 const Campus = require('../db/models/campus');
 
 router.get('/', async (req, res, next) => {
-  const campuses = await Campus.findAll();
-  res.json(campuses)
+  try {
+    const campuses = await Campus.findAll();
+    res.json(campuses)
+  } catch (err) { next(err) }
+
 })
 
 router.get('/:id', async (req, res, next) => {
-  const id = req.params.id;
-  const campus = await Campus.findOne({
-    where: {
-      id
-    }
-  })
-  res.json(campus)
+  try {
+    const id = req.params.id;
+    const campus = await Campus.findOne({
+      where: {
+        id
+      }
+    })
+    res.json(campus)
+  } catch (err) { next(err) }
 })
 
 router.post('/', async (req, res, next) => {
--- server/api/students.js
+++ server/api/students.js
@@ -2,18 +2,22 @@ const router = require('express').Router()
 const Student = require('../db/models/student')
 
 router.get('/', async (req, res, next) => {
-  const students = await Student.findAll();
-  res.json(students);
+  try {
+    const students = await Student.findAll();
+    res.json(students);
+  } catch (err) { next(err) }
 })
 
 router.get('/:id', async (req, res, next) => {
-  const id = req.params.id
-  const student = await Student.findOne({
-    where: {
-      id
-    }
-  })
-  res.json(student)
+  try {
+    const id = req.params.id
+    const student = await Student.findOne({
+      where: {
+        id
+      }
+    })
+    res.json(student)
+  } catch (err) { next(err) }
 })
 
 router.post('/', async (req, res, next) => {
