68a9d24baaf886b3283375eff892771bbe0b6f2c
Jami Gibbs <jami0821@gmail.com>
2018-05-19 07:25:04 -0500

    Creating Student model, closes #1

---
+++ server/db/models/students.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize')
+const db = require('../index')
+
+const Students = db.define('students', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    validate: {
+      isEmail: true,
+      allowNull: false
+    }
+  },
+  gpa: {
+    type: Sequelize.DECIMAL,
+    validate: {
+      min: 0.0,
+      max: 4.0
+    }
+  },
+  fullName: {
+    type: Sequelize.VIRTUAL,
+    get () {
+      return this.getDataValue('firstName') + ' ' + this.getDataValue('lastName')
+    }
+  }
+})
+
+module.exports = Students
