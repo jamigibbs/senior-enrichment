1fe78ff32ac59217414404b937cc83c847458287
Jami Gibbs <jami0821@gmail.com>
2018-05-19 10:01:14 -0500

    Adding campus POST route, closes #8

--- server/api/campuses.js
+++ server/api/campuses.js
@@ -16,4 +16,11 @@ router.get('/:id', async (req, res, next) => {
   res.json(campus)
 })
 
+router.post('/', async (req, res, next) => {
+  try {
+    const campus = await Campus.create(req.body);
+    res.json(campus)
+  } catch (err) { next(err) }
+})
+
 module.exports = router
