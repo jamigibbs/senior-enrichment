04ba85399e55d0b20db1f9bd4dad36f86d125932
Jami Gibbs <jami0821@gmail.com>
2018-05-21 10:18:38 -0500

    Home paging style

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div id="home">
-      <h1>Hello world! You're home.</h1>
+      <div className="container">
+        <div className="row">
+          <div className="column">
+            <h1>An Intergalactic Directory</h1>
+          </div>
+        </div>
+        <div className="row buttons-wrap">
+          <div className="column">
+            <Link className="button" to="/campuses">Campuses</Link>
+            <Link className="button" to="/students">Students</Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
