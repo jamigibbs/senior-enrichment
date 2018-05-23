68f066a81417adcfac7ac3a977c537fae172a2c5
Jami Gibbs <jami0821@gmail.com>
2018-05-23 17:41:16 -0500

    Campus view progress

--- app/components/Campuses/CampusView.js
+++ app/components/Campuses/CampusView.js
@@ -1,13 +1,61 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Loader from 'react-loading'
+import { fetchCampuses } from '../../reducers'
+import CampusCard from './CampusCard'
+import { StudentCard } from '../Students'
 
 export class CampusView extends Component {
+
+  componentDidMount(){
+    this.props.fetchCampuses()
+  }
+
+  findCampus = (school) => {
+    return school.id === Number(this.props.match.params.id)
+  }
+
   render(){
+    const { campuses, isFetching } = this.props;
+
+    if (isFetching) {
+      return <Loader className="preloader" type="balls" color="#9b4dca" />
+    }
+
+    const campus = campuses.find(this.findCampus)
+
     return (
       <div>
-        <h1>On the Campus View Page!</h1>
+        {
+          campus &&
+          <div>
+            <CampusCard campus={campus} />
+            {
+              campus.students.map( (student, index) => {
+                return <StudentCard key={index} student={student} campus={campus}/>
+              })
+            }
+          </div>
+        }
       </div>
     )
   }
 }
 
-export default CampusView
+const mapStateToProps = (state) => {
+  return {
+    campuses: state.campuses,
+    isFetching: state.isFetching
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchCampuses: () => {
+      dispatch(fetchCampuses())
+    }
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CampusView)
--- app/components/Students/index.js
+++ app/components/Students/index.js
@@ -1,3 +1,4 @@
 export { default as StudentsMain } from './StudentsMain'
 export { default as StudentAdd } from './StudentAdd'
 export { default as StudentView } from './StudentView'
+export { default as StudentCard } from './StudentCard'
